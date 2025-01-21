23232249d301f28443005e45a05bba4b120b8d03
favian medina gemio <fabinnerself@gmail.com>
2025-01-20 23:14:27 -0400

    ajust restrict role to some routes

--- src/presentation/repair/router.ts
+++ src/presentation/repair/router.ts
@@ -23,15 +23,16 @@ export class RepairRoutes {
     const repairController = new RepairController(repairService);
  
     router.use(AuthMiddleware.protect);
+    router.post("/", repairController.createRepair);        
+    router.post("/:id", repairController.noIdRepair);
+
     router.use(AuthMiddleware.restrictTo(UserRole.ADMIN));
     
     router.get("/", repairController.findAllRepairs);
     router.get("/:id", repairController.findOnerepair);
     router.delete("/", repairController.validRepair);
     router.delete("/:id", repairController.deleteRepair);
-    router.patch("/", repairController.validRepair);
-    router.post("/", repairController.createRepair);        
-    router.post("/:id", repairController.noIdRepair); 
+    router.patch("/", repairController.validRepair);     
     router.patch("/", repairController.validRepair);
     router.patch("/:id", repairController.updateRepair);
     
